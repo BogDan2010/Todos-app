e70af412c7f020e12c6ec7f71ad9d08e6fed5ac1
BogDan2010 <bogdan-2010@inbox.ru>
2022-02-01 18:04:44 +0300

    Added redirect in to the Home page.

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import { appRoutes } from "../routes"
 
 const AppRoutes = () => {
@@ -8,6 +8,7 @@ const AppRoutes = () => {
             {appRoutes.map(({ path, element }) => (
                 <Route key={path} path={path} element={element} exact />
             ))}
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
 }
